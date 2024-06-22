8a746da57005b4f6021c351c845bd4f6611786fe
wiaan <wiaan.botha@spandigital.com>
2024-06-22 13:28:46 +0200

    finished user api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,30 @@
 import express from 'express';
 
-import { createUser, getAllUsers } from './users';
+import { createUser, getAllUsers, getUserById, updateUser } from './users';
 
 const router = express.Router();
 
+// List User by ID
+router.get('/api/users/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    res.status(400);
+    res.json({ message: 'Invalid ID' });
+  }
+
+  const user = await getUserById(id);
+
+  if (!user.data) {
+    res.status(404);
+    res.json({ message: 'User not found' });
+  }
+
+  res.status(200);
+  res.json(user);
+});
+
+// List Users
 router.get('/api/users', async (req, res) => {
   const page = !isNaN(parseInt(`${req.query.page}`)) ? parseInt(`${req.query.page}`) : 1;
   let size = !isNaN(parseInt(`${req.query.size}`)) ? parseInt(`${req.query.size}`) : 10;
@@ -16,15 +37,36 @@ router.get('/api/users', async (req, res) => {
   res.json(users);
 });
 
+// Create User
 router.post('/api/users', async (req, res) => {
   const user = await createUser(req.body);
 
   if (user.error) {
-    res.status(404);
+    res.status(400);
     res.json({ message: user.error });
   }
 
   res.status(201);
+  res.json(user);
+});
+
+// Update User
+router.put('/api/users/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    res.status(400);
+    res.json({ message: 'Invalid ID' });
+  }
+
+  const user = await updateUser(id, req.body);
+
+  if (!user.data) {
+    res.status(404);
+    res.json({ message: 'User not found' });
+  }
+
+  res.status(200);
   res.json(user);
 });
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -9,8 +9,18 @@ type User = {
 
 export interface IResponse {
   error?: string;
-  data?: User | User[];
+  data?: User | User[] | null;
 }
+
+export const getUserById = async (id: number): Promise<IResponse> => {
+  const user: User | null = await Client.user.findUnique({
+    where: {
+      id
+    }
+  });
+
+  return { data: user };
+};
 
 export const getAllUsers = async (size: number, page: number): Promise<IResponse> => {
   const shouldSkip = page * size > size;
@@ -28,11 +38,7 @@ export const getAllUsers = async (size: number, page: number): Promise<IResponse
   return { data: users };
 };
 
-export const createUser = async (data: {
-  username: string;
-  first_name: string;
-  last_name: string;
-}): Promise<IResponse> => {
+export const createUser = async (data: User): Promise<IResponse> => {
   const { username, first_name, last_name } = data;
 
   if (username === '') return { error: 'Username is required' };
@@ -45,6 +51,27 @@ export const createUser = async (data: {
       first_name,
       last_name
     }
+  });
+
+  return {
+    data: res
+  };
+};
+
+export const updateUser = async (id: number, data: User): Promise<IResponse> => {
+  const user: User | null = await Client.user.findUnique({
+    where: {
+      id
+    }
+  });
+
+  if (!user) return { error: 'User not found' };
+
+  const res = await Client.user.update({
+    where: {
+      id
+    },
+    data
   });
 
   return {
